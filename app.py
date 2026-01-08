cb021aec64860f651709512bed2cf81226a9a530
Wellynton Martins <wellyntonjoaomartins@gmail.com>
2026-01-08 17:23:42 -0300

    Update 08/01/2026 (Deploy flask app to railway)

--- app.py
+++ app.py
@@ -392,5 +392,6 @@ def logout():
 
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=5000, debug=True)
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
